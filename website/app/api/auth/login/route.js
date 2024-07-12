3462ef0205aea0d40c8edbfd6e4312e52a517b1c
MagnusCarlsen26 <khushal.sindhav26@gmail.com>
2024-07-12 15:53:31 +0530

    Implemented login with JWT

---
+++ website/app/api/auth/login/route.js
@@ -0,0 +1,50 @@
+import jwt from 'jsonwebtoken'
+import { NextResponse } from 'next/server'
+import User from '@/lib/schemas/user';
+import { authDb } from '@/lib/mongo';
+import bcrypt from 'bcrypt'
+
+async function getuserTable(username,password) {
+    const record = await User.findOne({ username }) 
+    console.log(password)
+    if (record) {
+        if (await bcrypt.compare(password,record.password)) return "OK"
+        else return 'Incorrect Password'
+    }
+    return "No username found"
+}
+
+export async function POST(req,res) {
+    try{
+        await authDb()
+        const data = await req.json()
+        const { username,password } = data
+        
+        const check = await getuserTable(username,password)
+        console.log(check)
+        if (check !== "OK") return NextResponse.json({
+            success : false,
+            data : { message : check }
+        })
+
+        const jwt_secret = process.env.JWT_SECRET
+        const token = jwt.sign({
+            username,
+            password
+            },
+            jwt_secret,{
+                expiresIn : '1h'
+            }
+        )
+        return NextResponse.json({
+            success : true,
+            data : { token }
+        })
+    } catch (error) {
+        console.log(error.message)
+        return NextResponse.json({
+            success : false,
+            data : { error }
+        })
+    }
+}
--- website/components/login/login.jsx
+++ website/components/login/login.jsx
@@ -1,4 +1,28 @@
+"use client"
+
+import axios from "axios"
+import React , { useState } from "react"
+
 export default function Login() {
+    
+    const [username,setUsername] = useState("")
+    const [password,setPassword] = useState("")
+    const [error,setError] = useState("")
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        const response = await axios.post("http://localhost:3000/api/auth/login",{
+            username,
+            password
+        })
+        console.log(response.data)
+        if (response.data.success) {
+            localStorage.setItem("Authorization",(response.data.data.token))
+            console.log("Logged in")
+        } else {
+            setError(response.data.data.message)
+        }       
+    }
+
     return <div className="h-screen flex justify-center flex-col">
         <div className="flex justify-center">
         <a href="#" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 ">
@@ -9,19 +33,20 @@ export default function Login() {
                         </div>
                     </div>
                     <div className="pt-2">
-                        <LabelledInput label="Username" placeholder="Username" />
-                        <LabelledInput label="Password" type={"password"} placeholder="Password" />
-                        <button type="button" className="mt-8 w-full text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Sign in</button>
+                        <LabelledInput label="Username" placeholder="Username" onChange={(e) => setUsername(prev => e.target.value)}/>
+                        <LabelledInput label="Password" type={"password"} placeholder="Password" onChange={(e) => setPassword(prev => e.target.value)}/>
+                        <button type="button"  onClick={handleSubmit} className="mt-8 w-full text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Sign in</button>
                     </div>
+                    { error }
                 </div>
             </a>
         </div>
     </div>
 }
 
-function LabelledInput({ label, placeholder, type }) {
+function LabelledInput({ label, placeholder, type,onChange }) {
     return <div>
         <label className="block mb-2 text-sm text-black font-semibold pt-4">{label}</label>
-        <input type={type || "text"} id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder={placeholder} required />
+        <input type={type || "text"} id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder={placeholder} onChange={onChange} required />
     </div>
 }
